028776ceba607eeeca6d2cfd60ed506115fa58f9
Eva Pace <eba.pachi@gmail.com>
2024-04-21 16:48:56 -0300

    use rustyline lib for better cli experience

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs, io::{self, Write}, os::unix::fs::MetadataExt};
+use std::{collections::HashMap, fs, io, os::unix::fs::MetadataExt};
 
 #[derive(Debug)]
 enum Cmd {
@@ -22,6 +22,8 @@ impl Cmd {
     }
 }
 
+use rustyline::error::ReadlineError;
+
 fn main() -> io::Result<()> {
     // this is bad only for windows
     // but who cares...
@@ -30,27 +32,42 @@ fn main() -> io::Result<()> {
     folder.push(".raiden");
     let mut file = folder.clone();
     file.push("data");
+    let mut history = folder.clone();
+    history.push("history.txt");
 
     let exists = std::path::Path::new(&file).exists();
     if !exists {
         fs::create_dir_all(folder)?;
         fs::File::create(&file)?;
+        fs::File::create(&history)?;
     }
 
     let size = fs::metadata(&file)?.size();
     let mut store = if size == 0 {
+        fs::write(&file, "{}")?;
         HashMap::new()
     } else {
         let contents = fs::read_to_string(&file)?;
         ron::from_str(&contents).unwrap()
     };
 
-    let stdin = io::stdin();
-    let mut input = String::new();
+    let mut rl = rustyline::DefaultEditor::new().unwrap();
+    let _ = rl.load_history(&history);
+
     loop {
-        input.clear();
-        io::stdout().flush()?;
-        stdin.read_line(&mut input)?;
+        let input = match rl.readline("> ") {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str()).unwrap();
+                line
+            },
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
+                break
+            },
+            Err(err) => {
+                println!("error: {:?}", err);
+                break
+            }
+        };
         let cmd = Cmd::parse(&input);
         match cmd {
             Some(Cmd::Get(k)) => {
@@ -70,5 +87,8 @@ fn main() -> io::Result<()> {
             },
             None => {},
         };
-    }
+    };
+
+    let _ = rl.save_history(&history);
+    Ok(())
 }
