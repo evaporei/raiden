26ff62e93ddc1cc1e0881e001edf1601f9d0e63f
Eva Pace <eba.pachi@gmail.com>
2024-04-21 12:34:56 -0300

    basic stdin repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    let stdin = io::stdin();
+    loop {
+        input.clear();
+        io::stdout().flush()?;
+        stdin.read_line(&mut input)?;
+        print!("{input}");
+    }
 }
