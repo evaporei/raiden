5dab0a183b5d1777bc0db5d2be2c3b82a981bce8
Eva Pace <eba.pachi@gmail.com>
2024-04-21 12:44:42 -0300

    parse cmd get set

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
 use std::io::{self, Write};
 
+#[derive(Debug)]
+enum Cmd {
+    Get(String),
+    Set(String, String),
+}
+
+impl Cmd {
+    fn parse(input: &str) -> Option<Self> {
+        let mut words = input.split_whitespace();
+        match words.next() {
+            Some("get") => Some(Self::Get(words.next()?.to_owned())),
+            Some("set") => {
+                let mut kv = words.next().map(|kv| kv.split('='))?;
+                let k = kv.next()?.to_owned();
+                let v = kv.next()?.to_owned();
+                Some(Self::Set(k, v))
+            },
+            Some(_) | None => None
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     let mut input = String::new();
     let stdin = io::stdin();
@@ -7,6 +29,6 @@ fn main() -> io::Result<()> {
         input.clear();
         io::stdout().flush()?;
         stdin.read_line(&mut input)?;
-        print!("{input}");
+        println!("{:?}", Cmd::parse(&input));
     }
 }
